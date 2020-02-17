463adc08409137c80c24b352aa5b9e92afd7ea05
mcdieg <diego@libbheros.fr>
2020-02-17 22:01:51 +0100

    changed text

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -90,18 +90,18 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      L'équipe
                     </Link>
                   </div>
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Dernières actualités
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
-                      Read more
+                      Continuer
                     </Link>
                   </div>
                 </div>
