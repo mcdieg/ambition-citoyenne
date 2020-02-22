991ee08d49f05b83b9fbb5197b41a9e272bb247e
mcdieg <diego@libbheros.fr>
2020-02-22 09:43:54 +0100

    fixd txt

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -89,7 +89,7 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Dernières actualités
+                    Nos actualités
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
