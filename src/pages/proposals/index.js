88d95d33469573552982d705a8cbae43ac2115f7
mcdieg <diego@libbheros.fr>
2020-02-25 20:09:49 +0100

    added content thanks page

--- src/pages/proposals/index.js
+++ src/pages/proposals/index.js
@@ -11,8 +11,49 @@ export default () => (
           <p>Un programme est en cours de construction avec le collectif de citoyens constitué suite à notre réunion du 1er février 2020.
           En attendant nous vous rappelons quelques tribunes éditées mensuellement dans le journal de Robinson et qui sont rédigées par nos 4 élus : Jean-François Papot, Béatrice Maubras, Christophe Leroy et Catherine Faguette.
           </p>
+          <h1>Manifeste pour un Plessis-Robinson à la hauteur des Robinsonnais-es</h1>
+          <h2>Développer la nature en ville</h2>
+          <p>
+            Le Plessis-Robinson doit devenir la ville verte à laquelle aspirent tous les Robinsonnais. Pour cela, nous proposons qu’au moins le tiers des 70 Hectares d’aménagements planifiés sur le secteur Noveos devienne une forêt urbaine en pleine terre de part et d’autre des lignes à Très Haute Tension enterrées. 
+            Nouveau poumon vert public de la commune, cet espace ponctué d’espaces dédiés à la jeunesse et de jardins partagés limitera les risques liés à l’implantation de logements à proximité des lignes THT mais permettra aussi de gérer naturellement les eaux de pluie liées à la sur-urbanisation du quartier Ledoux et des nombreuses parcelles vendues par l’actuelle majorité à des promoteurs. 
+            En parallèle, nous proposons que chacun obtienne un droit à végétaliser les rues en complément d’un fleurissement communal redevenu naturel, c'est-à-dire ne nécessitant que peu d’eau l’été et composé d’espèces vivaces.
+          </p> 
+          <h2>Mettre en selle le plan vélo </h2>
+          <p>
+            Le relief de la ville n’est pas le principal frein au développement des mobilités actives. Nous devons compléter les subventions existantes en faveur des vélos électriques, mettre en place des pistes cyclables continues et sécurisées sur les grands axes complétées par des zones de rencontre sur les routes communales ; définir et déployer une signalisation cycliste et pédestre dans la commune ; proposer des attaches vélos devant tous les équipements et espaces publics ; développer des services numériques et physiques pour la location, la réparation … et favoriser des ateliers vélos dans toutes les nouvelles constructions.
+          </p>
+          <h2>Donner sa place à notre jeunesse</h2> 
+          <p>
+            Notre ville est riche de sa jeunesse. La politique municipale doit construire l’avenir avec et pour les jeunes et pour cela commencer par : rétablir le pacte éducatif avec les parents, les éducateurs, les jeunes, les associations ; rendre prioritaire le programme de réhabilitation-rénovation en faveur des enfants de l’école François Peatrik mais aussi de Louis Pergaud et Henri Wallon ; donner enfin un terrain pour agrandir le lycée Montesquieu ou en bâtir un nouveau …
+          </p>
+          <h2>Intégrer l’urgence climatique</h2> 
+          <p>
+            Notre commune doit devenir le moteur de la transition environnementale et agir pour cela à tous les niveaux avec une réelle brigade de l’environnement. Le coût des terrains pour les promoteurs doit être indexé sur la sobriété énergétique et l’exemplarité environnementale des bâtiments qu’ils proposent ; un service « habitat /climat » doit être mis en place et offrir ainsi aux copropriétés l’ingénierie technique et financière nécessaire aux rénovations et leur proposer des sur-élévations dans les quartiers proches de la gare ou des tramways ; l’ensemble du parc automobile de la commune doit passer à l’électrique. Les réseaux de chaleur vertueux (alimentés par la géothermie et/ou par de nouvelles chaufferies biomasse) doivent être développés. Les équipements publics, en lien au besoin avec des tiers investisseurs, doivent devenir des bâtiments à énergie positive par un déploiement massif de panneaux photovoltaïques.
+          </p>
+          <h2>Remettre à plat le budget</h2>
+          <p>
+            Malgré les hausses d’impôts et des coûts des services municipaux (pour le sport, le périscolaire ou la culture), les finances de la ville se dégradent. 
+            Pourtant des marges de manœuvre existent avec des dépenses par habitant bien plus élevées que la moyenne comme le démontrerait l’audit de la cour des comptes que nous réclamons en vain depuis 2014. 
+            Ainsi il est possible de limiter les recours coûteux aux prestataires (pour les espaces verts ou pour la restauration collective pour des repas plus sains et plus écologiques); réévaluer le montant des subventions aux différentes associations ; auditer le PPP-Voirie et son coût pour la ville ; ouvrir un débat sur le stationnement payant (en veillant à mettre en place des cartes résidents) ; réviser les concessions du marché et du parking de cœur de ville qui doivent devenir des sources de revenu pour la ville ; stabiliser puis réduire la dette qui n’est plus soutenable et conduit à une politique foncière spéculative.
+          </p>
+          <h2>Construire une ville mixte</h2>
+          <p>
+            Le Plessis peut être réellement mixte et solidaire. Il est possible d’assurer un équilibre dans tous les programmes de logements collectifs neufs (avec un tiers de logement locatif social, un tiers de logement en accession à prix maitrisés et un tiers en accession libre). La ville doit lutter contre la hausse disproportionnée des prix de l’immobilier en vendant ses terrains au juste prix. Notre commune peut favoriser la mixité des usages et donc l’implantation de TPE/PME dans la ville pour offrir une diversité d’emplois locaux aux Robinsonnais. Le quotient familial appliqué à tous les services facturés par la ville garantit un accès équitable au sport, à la culture…
+            Faire des Robinsonnais des citoyens actifs
+            Tous les documents utiles doivent être disponibles sur internet (comptes rendus des débats, tribunes, budgets, décisions…). Des conseils de quartiers pourraient disposer de budgets participatifs et des commissions municipales garantir l’équité dans l’attribution des logements sociaux et des places en crèche. Un jury citoyen pourrait voter sur tous les équipements et nouveaux logements, tandis que le conseil municipal doit rendre possible l’interpellation citoyenne.
+            
+            Ces éléments ne sont pas exhaustifs. Pragmatiques et finançables, ils montrent que l’avenir peut être enthousiasmant pour notre ville et faire du Plessis-Robinson une ville durable et conviviale… En attente de cet avenir désirable, nous vous souhaitons d’excellentes fêtes de fin d’année.
+            
+            Vos élu-e-s de l'opposition :
+            JF PAPOT, B MAUBRAS,C LEROY, C FAGUETTE
+            Permanences : le samedi matin au 45 avenue Herriot ou le dimanche matin au marché – hors congés
+          </p>
         </div>
       </div>
     </section>
   </Layout>
 )
+
+
+
+ 
